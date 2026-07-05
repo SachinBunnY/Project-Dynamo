e88d446df92551c385630b8b1f2cb37b6d60c7c1
Sachin Prasad <sachingamer97@gmail.com>
2026-07-05 19:16:49 +0530

    Fixed the problem

--- log-report/tests/test.sh
+++ log-report/tests/test.sh
@@ -4,10 +4,12 @@ set -uo pipefail
 pytest /tests/test_outputs.py -rA
 status=$?
 
+mkdir -p /logs/verifier
+
 if [ "$status" -eq 0 ]; then
-    echo 1 > /app/reward.txt
+    echo 1 > /logs/verifier/reward.txt
 else
-    echo 0 > /app/reward.txt
+    echo 0 > /logs/verifier/reward.txt
 fi
 
 exit "$status"
